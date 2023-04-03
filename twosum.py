e765e021110d22ecd5f8ff3aa5b8acc918c41856
Phuwasit Chokwiriyadit <inkkyhero@gmail.com>
2023-04-03 14:30:49 +0700

    del hello

--- twosum.py
+++ twosum.py
@@ -18,4 +18,4 @@ solution = Solution()
 print(solution.twoSum([2, 7, 11, 15], 9))  # Output: [0, 1]
 print(solution.twoSum([3, 2, 4], 6))       # Output: [1, 2]
 print(solution.twoSum([3, 3], 6))          # Output: [0, 1]
-print('https://leetcode.com/problems/two-sum/', 'hello world=1111')
+print('https://leetcode.com/problems/two-sum/', 'hello world=11115')
