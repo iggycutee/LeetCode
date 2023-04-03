a67dfc2a4d84ba4516a595cf8d5b970dee93cf2a
Phuwasit Chokwiriyadit <inkkyhero@gmail.com>
2023-04-03 12:26:43 +0700

    first commit

---
+++ twosum.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_index = {}
+        
+        for i, num in enumerate(nums):
+            diff = target - num
+            
+            if diff in num_index:
+                return [num_index[diff], i]
+            
+            num_index[num] = i
+        
+        return None
+
+solution = Solution()
+print(solution.twoSum([2, 7, 11, 15], 9))  # Output: [0, 1]
+print(solution.twoSum([3, 2, 4], 6))       # Output: [1, 2]
+print(solution.twoSum([3, 3], 6))          # Output: [0, 1]
